e3815be54fdc2d97f9f6a1ecc064b2c7d450e0e6
Marcelo Medeiros de Lima <marcelolima381@outlook.com>
2016-05-30 16:13:42 -0300

    .

---
+++ Javascript/exe2.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".numerar").click(function() {
+        var ul = $("ul.list-group");
+        var li = $("ul.list-group").children();
+        for (var i = 0; i < li.length; i++) {
+            var element = li[i];
+            $(element).html("[" + (i+1) + "]" + $(element).html())
+        }
+    });
+});
