f3d4226caa5019d1e72ce50e43dfe378da817157
Marcelo Medeiros de Lima <marcelolima381@outlook.com>
2016-06-10 08:18:07 -0300

    Correção de bugs
    Correção de bugs no exercício 7

--- Javascript/exe7.js
+++ Javascript/exe7.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
     $("#cpf").blur(function(){
         var cpf = $(this).val();
-        for (var i = 0; i < cpf.length; i++) {
-            if (cpf.charCodeAt(i) >= 48 && cpf.charCodeAt(i) <= 57) {
-                $("div.form-group").removeClass("has-error");
-                $("div.form-group").addClass("has-success");
+        var finalResult = true;
+        if (cpf.length != 11 && cpf.length != 14) {
+            $(this).val("USE SOMENTE NÚMEROS");
+            $("div.form-group").addClass("has-error");
+            $("div.form-group").removeClass("has-success");
+            finalResult = false;
+        }
+        else {
+            for (var i = 0; i < cpf.length; i++) {
+                if (cpf.charCodeAt(i) >= 48 && cpf.charCodeAt(i) <= 57 && finalResult == true) {
+                    $("div.form-group").addClass("has-success");
+                    $("div.form-group").removeClass("has-error");
+                    finalResult = true;
+                }
+                else {
+                    $(this).val("USE SOMENTE NÚMEROS");
+                    $("div.form-group").addClass("has-error");
+                    $("div.form-group").removeClass("has-success");
+                    finalResult = false;
+                }
+            }
+            if (finalResult == true && cpf.length != 14) {
                 $(this).val(cpf[0] + cpf[1] + cpf[2] + "." + cpf[3] + cpf[4] + cpf[5] + "." + cpf[6] + cpf[7] + cpf[8] + "-" + cpf[9] + cpf[10]);
             }
-            else {
-                $(this).val("USE SOMENTE NÚMEROS");
-                $("div.form-group").removeClass("has-success");
-                $("div.form-group").addClass("has-error");
-            }
         }
+
     });
 });
+// $(this).val(cpf[0] + cpf[1] + cpf[2] + "." + cpf[3] + cpf[4] + cpf[5] + "." + cpf[6] + cpf[7] + cpf[8] + "-" + cpf[9] + cpf[10]);
